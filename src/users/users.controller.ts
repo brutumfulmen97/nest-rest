4887b268a5dca51b7f2f772a410fb57ae3ae3689
Vlatko <vlatkooop@gmail.com>
2024-11-30 23:55:03 +0100

    setup mongo and users crud operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,51 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
-  getUsers() {
-    return 'get users';
+  getUsers(
+    @Query('sort') sort: 'asc' | 'desc' = 'desc',
+    @Query('limit') limit: number = 10,
+    @Query('role') role: 'ADMIN' | 'USER' = 'USER',
+  ) {
+    return this.usersService.getUsers(sort, limit, role);
   }
 
   @Get('admins')
   getAdmins() {
-    return 'get admins';
+    return this.usersService.getAdmins();
   }
 
   @Get(':id')
   getUser(@Param('id') id: string) {
-    return `get user ${id}`;
+    return this.usersService.getUser(id);
+  }
+
+  @Post()
+  createUser(@Body() body: User) {
+    return this.usersService.createUser(body);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: Partial<User>) {
+    return this.usersService.updateUser(id, body);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  getUsers(
+    sort: 'asc' | 'desc' = 'desc',
+    limit: number = 10,
+    role: 'ADMIN' | 'USER' = 'USER',
+  ) {
+    return this.userModel.find({ role }).sort({ createdAt: sort }).limit(limit);
+  }
+
+  getUser(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  createUser(body: User) {
+    return this.userModel.create(body);
+  }
+
+  updateUser(id: string, body: Partial<User>) {
+    return this.userModel.findByIdAndUpdate(id, body);
+  }
+
+  deleteUser(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+
+  getAdmins() {
+    return this.userModel.find({ role: 'ADMIN' });
+  }
+}
