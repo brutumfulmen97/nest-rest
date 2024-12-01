33428dacb36196bd30b9bd5c82b37c76c14ef63e
Vlatko <vlatkooop@gmail.com>
2024-12-01 12:23:18 +0100

    added nunjucks templating and tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import * as hbs from 'hbs';
+
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const express = app.getHttpAdapter().getInstance();
 
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
-    prefix: '/',
-  });
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.set('view options', { layout: 'layouts/layout' });
-  app.setViewEngine('hbs');
-  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+  const nunjucksOptions: nunjucks.ConfigureOptions = {
+    autoescape: true,
+    throwOnUndefined: false,
+    trimBlocks: false,
+    lstripBlocks: false,
+    watch: true,
+    noCache: process.env.NODE_ENV === 'local' ? true : false,
+    express: app,
+  };
+
+  const views = join(__dirname, '..', 'views');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(views);
+  nunjucks.configure(views, { express });
+
+  const viewEnv = nunjucks.configure(views, nunjucksOptions);
+  app.engine('njk', viewEnv.render);
+  app.setViewEngine('njk');
 
   await app.listen(process.env.PORT ?? 3000);
 }
---
+++ tailwind.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+npx tailwindcss -i ./public/tailwind.css -o ./public/style.css --watch
